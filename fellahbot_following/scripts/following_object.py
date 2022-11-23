4efb485dc974e26f657ab6c788ac3bb7452b45fa
jeremie-ntchouala <jeremie-ntchouala@github.com>
2022-11-23 16:46:40 +0100

    Create fellahbot_following pkgs

---
+++ fellahbot_following/scripts/following_object.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# @Project_name: catkin_ws
+# @File_name:    following_object.py
+# @User:         jeremie
+# @Author:       Jeremie Ntchouala
+# @Email:        jeremie.ntchouala@fellahtech.com
+# @Time:         23 novembre 2022 at 16:41
+
+from __future__ import print_function
+from __future__ import division
+
+import sys
+
+
+def main(args):
+    pass
+
+
+if __name__ == '__main__':
+    main(sys.argv)
