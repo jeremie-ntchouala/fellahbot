f600dc9871479b4e337bbf87db9023be7dcc8c2a
jeremie-ntchouala <jeremie-ntchouala@github.com>
2022-11-23 16:51:00 +0100

    create and editing the following driver modules

--- fellahbot_following/modules/following_driver.py
+++ fellahbot_following/modules/following_driver.py
@@ -13,8 +13,78 @@ from __future__ import division
 import sys
 
 
+class FollowingDriver:
+    def __init__(self, max_speed=10, max_steering=30):
+        """
+        Init communication, set default settings, ...
+        """
+        self.max_steering = max_steering
+        self.max_speed = max_speed
+        self.current_speed = 0
+        self.current_steering = 0
+
+        self.voltage = 12
+        self.temperature = 47
+
+    def __del__(self):
+        """
+        Close communication, and destroy all settings, ...
+        """
+        print("closing")
+
+    def set_speed(self, speed):
+        """
+        Give a speed that the motor will try to reach.
+        """
+        if speed <= self.max_speed:
+            self.current_speed = speed
+        else:
+            self.current_speed = self.max_speed
+
+    def set_steering(self, steering):
+        """
+        Give a speed that the motor will try to reach.
+        """
+        if steering <= self.max_steering:
+            self.current_steering = steering
+        else:
+            self.current_steering = self.max_steering
+
+    def stop(self):
+        """
+        Set speed to 0 and thus stop the motor
+        """
+        self.current_speed = 0
+        self.current_steering = 0
+        print("STOP")
+
+    def get_speed(self):
+        """
+        Return current speed
+        """
+        return self.current_speed
+
+    def get_steering(self):
+        """
+        Return current speed
+        """
+        return self.current_steering
+
+    def get_status(self):
+        """
+        Get hardware information from the motor
+        """
+        return {
+            'temperature': self.temperature,
+            'voltage': self.voltage,
+            'speed': self.get_speed(),
+            'steering': self.get_steering(),
+        }
+
+
 def main(args):
-    pass
+    test = FollowingDriver()
+    print(test.get_status())
 
 
 if __name__ == '__main__':
