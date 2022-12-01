726b050c3d0411c9ecdf723186a4b7042531c0aa
jeremie-ntchouala <jeremie-ntchouala@github.com>
2022-12-01 21:57:59 +0100

    adding def bounding box to estimate objects area

--- fellahbot_following/include/fellahbot_following/image_processing.py
+++ fellahbot_following/include/fellahbot_following/image_processing.py
@@ -13,6 +13,7 @@ from __future__ import division
 import sys
 import cv2
 import imutils
+import numpy as np
 
 
 class ImageProcessing:
@@ -21,6 +22,19 @@ class ImageProcessing:
         self.upper = upper  # The Upper limit of the colour range in the form of RGB Values
         self.radius = None  # The radius of the Circle drawn around the object
         self.center = None  # The center of the circle drawn around the object
+
+    def bounding_box(self, contours):
+        box = []
+        peri = cv2.arcLength(contours, True)
+        approx = cv2.approxPolyDP(contours, 0.04 * peri, True)  # 0.05 or 0.04
+        if len(approx) == 4:
+            target = approx
+            box = cv2.boundingRect(approx)
+
+        return box
+
+    def rect_to_point(self, box):
+        return
 
     def process_image(self, frame):
         self.frame = frame
@@ -41,19 +55,46 @@ class ImageProcessing:
 
         if len(cnts) > 0:
             c = max(cnts, key=cv2.contourArea)
+
+            # Bounding rectangle version 1
+            bbox = self.bounding_box(c)
+            # Bounding rectangle version 2
+            rect = cv2.minAreaRect(c)
+            box = cv2.boxPoints(rect)
+            box = np.int0(box)
+
             ((x, y), self.radius) = cv2.minEnclosingCircle(c)
             M = cv2.moments(c)
             self.center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
             if self.radius > 10:
-                cv2.circle(self.frame, (int(x), int(y)), int(self.radius),
-                           (0, 255, 255), 2)
+                cv2.circle(self.frame, (int(x), int(y)), int(self.radius), (0, 255, 255), 2)
                 cv2.circle(frame, self.center, 5, (0, 0, 255), -1)
+                # cv2.drawContours(frame, [box], -1, (255, 0, 255), 1)
+                cv2.rectangle(frame, (bbox[0], bbox[1]), ((bbox[0]+bbox[2]),(bbox[1]+bbox[3])), (255, 0, 255), 1)
+                
 
-        return [frame, mask, self.center, self.radius]
+        return [frame, mask, self.center, self.radius, c, bbox, box]
 
 
 def main(args):
-    pass
+    image = cv2.imread("/home/jeremie/Frame_3.png")
+    # while 1:
+    sc = ImageProcessing((29, 86, 6), (64, 255, 255))
+    result = sc.process_image(image)
+    cv2.imshow("Frame", result[0])
+    cv2.imshow("Mask", result[1])
+    cv2.waitKey(1)
+    print('center ', result[2])
+    print('radius ', result[3])
+    print('bounding box v1 ', result[5])
+    print(result[0].shape)
+    print('bounding box v2', result[6])
+
+    if (cv2.waitKey(1) & 0xFF) == ord('q'):
+        pass
+        #break
+
+    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
