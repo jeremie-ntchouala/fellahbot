2ffb2567eb3cb6c998366339eea3becf177ade26
jeremie-ntchouala <jeremie-ntchouala@github.com>
2022-12-05 04:02:49 +0100

    adding box area data as output of image processing

--- fellahbot_following/include/fellahbot_following/image_processing.py
+++ fellahbot_following/include/fellahbot_following/image_processing.py
@@ -16,7 +16,7 @@ import imutils
 import numpy as np
 
 
-class ImageProcessing:
+class ImageProcessing():
     def __init__(self, lower=(29, 86, 6), upper=(64, 255, 255)):
         self.lower = lower  # The lower limit of the colour range in the form of RGB Values
         self.upper = upper  # The Upper limit of the colour range in the form of RGB Values
@@ -60,20 +60,27 @@ class ImageProcessing:
             bbox = self.bounding_box(c)
             # Bounding rectangle version 2
             rect = cv2.minAreaRect(c)
+            rect_width = rect[1][0]
+            rect_height = rect[1][1]
             box = cv2.boxPoints(rect)
             box = np.int0(box)
 
             ((x, y), self.radius) = cv2.minEnclosingCircle(c)
             M = cv2.moments(c)
-            self.center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+            if (np.int0(M["m00"]) !=0 ):
+                self.center = (np.int0(M["m10"] / M["m00"]), np.int0(M["m01"] / M["m00"]))
+            else :
+                self.center = (np.int0(x), np.int0(y))
+            self.area = np.int0(rect_width * rect_height)
+
             if self.radius > 10:
-                cv2.circle(self.frame, (int(x), int(y)), int(self.radius), (0, 255, 255), 2)
+                cv2.circle(self.frame, (np.int0(x), np.int0(y)), np.int0(self.radius), (0, 255, 255), 2)
                 cv2.circle(frame, self.center, 5, (0, 0, 255), -1)
-                # cv2.drawContours(frame, [box], -1, (255, 0, 255), 1)
-                cv2.rectangle(frame, (bbox[0], bbox[1]), ((bbox[0]+bbox[2]),(bbox[1]+bbox[3])), (255, 0, 255), 1)
+                cv2.drawContours(frame, [box], -1, (255, 0, 255), 1)
+                # cv2.rectangle(frame, (bbox[0], bbox[1]), ((bbox[0]+bbox[2]),(bbox[1]+bbox[3])), (255, 0, 255), 1)
                 
 
-        return [frame, mask, self.center, self.radius, bbox, box]
+        return [frame, mask, self.center, self.radius, bbox, box, self.area]
 
 
 def main(args):
@@ -89,6 +96,8 @@ def main(args):
     print('bounding box v1 ', result[4])
     print(result[0].shape)
     print('bounding box v2', result[5])
+    print('Box Area :', result[6])
+    print('center :', result[2])
 
     if (cv2.waitKey(1) & 0xFF) == ord('q'):
         pass
