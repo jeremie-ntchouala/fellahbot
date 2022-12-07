05fe6449bbd7fc87ab95a3419dc8a8ed084c7f7f
jeremie-ntchouala <jeremie-ntchouala@github.com>
2022-12-07 23:41:46 +0100

    changing class init, adding limit in set method

--- fellahbot_following/include/fellahbot_following/following_driver.py
+++ fellahbot_following/include/fellahbot_following/following_driver.py
@@ -12,26 +12,28 @@ from __future__ import division
 
 import sys
 import rospy
+import numpy as np
 from fellahbot_following.controller_PID import ControllerPID
 from fellahbot_following.image_processing import ImageProcessing
 
 
-class FollowingDriver:
-    def __init__(self, max_speed=10, max_steering=30):
+class FollowingDriver():
+    def __init__(self, max_linear_speed=3, max_steering_speed=0.5, target=[25000, 320], Proportional=None, Integrator=None, Derivator=None ):
         """
         Init communication, set default settings, ...
         """
-        self.max_speed = max_speed
-        self.max_steering = max_steering
-        self.current_speed = 0
-        self.current_steering = 0
+        self.max_linear_speed = max_linear_speed
+        self.max_steering_speed = max_steering_speed
+        self.current_linear_speed = 0
+        self.current_steering_speed = 0
 
-        self.voltage = 48
-        self.temperature = 37
-        self.target_area = 25000
-        self.target_center = 320
-        self.PID_controller = ControllerPID([self.target_area, self.target_center], [
-                                            0.0000045, 0.002], [0, 0], [0.000001, 0.001])
+        self.Proportional = Proportional
+        self.Integrator = Integrator
+        self.Derivator = Derivator
+        self.target_area , self.target_center = target
+
+        # ControllerPID.__init__(target, Proportional, Integrator, Derivator)
+        self.PID_controller = ControllerPID(target, Proportional, Integrator, Derivator)
 
     def __del__(self):
         """
@@ -39,64 +41,62 @@ class FollowingDriver:
         """
         rospy.loginfo("Deleting the following driver object")
 
-    def compute_control(self, frame):
-        # Creating an object of the class Image processing to process the incoming image
-        sc = ImageProcessing()
-        # process Image to detect object in the image
-        result = sc.process_image(self.cv_image)
-        x_length = result[0].shape[1]
-        x = int(x_length/2)    # Center of the image
-        print(result[2])
+    def compute_control(self, info):
+        [uncliped_lin_speed, uncliped_ang_speed] = self.PID_controller.update(info)
+        # clip these speeds to be less then the maximal speed specified above
+        linear_vel  = np.clip(uncliped_lin_speed, -self.max_linear_speed, self.max_linear_speed)
+        angular_vel = np.clip(uncliped_ang_speed, -self.max_steering_speed, self.max_steering_speed)
+        self.set_speed(linear_vel)
+        self.set_steering(angular_vel)
 
     def set_speed(self, speed):
         """
         Give a speed that the motor will try to reach.
         """
-        if speed <= self.max_speed:
-            self.current_speed = speed
+        if abs(speed) <= self.max_linear_speed:
+            self.current_linear_speed = speed
         else:
-            self.current_speed = self.max_speed
+            self.current_linear_speed = self.max_linear_speed
 
     def set_steering(self, steering):
         """
         Give a speed that the motor will try to reach.
         """
-        if steering <= self.max_steering:
-            self.current_steering = steering
+        if abs(steering) <= self.max_steering_speed:
+            self.current_steering_speed = steering
         else:
-            self.current_steering = self.max_steering
+            self.current_steering_speed = self.max_steering_speed
 
     def stop(self):
         """
         Set speed to 0 and thus stop the motor
         """
-        self.current_speed = 0
-        self.current_steering = 0
+        self.current_linear_speed = 0
+        self.current_steering_speed = 0
         rospy.loginfo("Stopping the driver controller")
 
     def get_speed(self):
         """
         Return current speed
         """
-        return self.current_speed
+        return self.current_linear_speed
 
     def get_steering(self):
         """
         Return current speed
         """
-        return self.current_steering
+        return self.current_steering_speed
 
     def get_status(self):
         """
         Get hardware information from the motor
         """
         return {
-            'temperature': self.temperature,
-            'voltage': self.voltage,
-            'max_speed': self.max_speed,
-            'max_steering': self.max_steering,
-            'current_speed': self.get_speed(),
-            'current_steering': self.get_steering(),
+            'PID param': 'P:{}, I:{}, D:{}'.format(self.Proportional, self.Integrator, self.Derivator), 
+            'max_linear_speed': self.max_linear_speed,
+            'max_steering_speed': self.max_steering_speed,
+            'current_linear_speed': self.get_speed(),
+            'current_steering_speed': self.get_steering(),
             'target_center':  self.target_area,
             'target_center': self.target_center,
 
