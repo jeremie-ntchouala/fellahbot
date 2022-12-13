1cad0af5598c1841156ad0fd1a8bd9b771c29fa2
jeremie-ntchouala <jeremie-ntchouala@github.com>
2022-12-13 17:11:23 +0100

    None type safety

--- fellahbot_following/include/fellahbot_following/image_processing.py
+++ fellahbot_following/include/fellahbot_following/image_processing.py
@@ -22,14 +22,14 @@ class ImageProcessing():
         self.upper = upper  # The Upper limit of the colour range in the form of RGB Values
         self.radius = None  # The radius of the Circle drawn around the object
         self.center = None  # The center of the circle drawn around the object
+        self.area = None    # The area of the box drawn around the object
+        self.frame = None
 
     def bounding_box(self, contours):
         box = []
         peri = cv2.arcLength(contours, True)
-        approx = cv2.approxPolyDP(contours, 0.04 * peri, True)  # 0.05 or 0.04
-        if len(approx) == 4:
-            target = approx
-            box = cv2.boundingRect(approx)
+        approx = cv2.approxPolyDP(contours, 0.004 * peri, True)  # 0.05 or 0.04
+        box = cv2.boundingRect(approx)
 
         return box
 
@@ -38,7 +38,7 @@ class ImageProcessing():
 
     def process_image(self, frame):
         self.frame = frame
-
+        
         blurred = cv2.GaussianBlur(self.frame, (11, 11), 0)
 
         hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
@@ -54,7 +54,7 @@ class ImageProcessing():
         cnts = imutils.grab_contours(cnts)
 
         if len(cnts) > 0:
-            c = max(cnts, key=cv2.contourArea)
+            c = max(cnts, key=cv2.contourArea)  # Maximum countour area
 
             # Bounding rectangle version 1
             bbox = self.bounding_box(c)
@@ -62,32 +62,42 @@ class ImageProcessing():
             rect = cv2.minAreaRect(c)
             rect_width = rect[1][0]
             rect_height = rect[1][1]
-            box = cv2.boxPoints(rect)
-            box = np.int0(box)
+            self.area = np.int0(rect_width * rect_height)
+            box = cv2.boxPoints(rect).astype('int')
+            # box = np.int0(box)
 
             ((x, y), self.radius) = cv2.minEnclosingCircle(c)
             M = cv2.moments(c)
-            if (np.int0(M["m00"]) !=0 ):
-                self.center = (np.int0(M["m10"] / M["m00"]), np.int0(M["m01"] / M["m00"]))
-            else :
+            if np.int0(M["m00"]) != 0:
+                self.center = (np.int0(M["m10"] / M["m00"]),
+                               np.int0(M["m01"] / M["m00"]))
+            else:
                 self.center = (np.int0(x), np.int0(y))
-            self.area = np.int0(rect_width * rect_height)
 
             if self.radius > 10:
-                cv2.circle(self.frame, (np.int0(x), np.int0(y)), np.int0(self.radius), (0, 255, 255), 2)
+                cv2.circle(self.frame, (np.int0(x), np.int0(y)),np.int0(self.radius), (0, 255, 255), 2)
                 cv2.circle(frame, self.center, 5, (0, 0, 255), -1)
                 cv2.drawContours(frame, [box], -1, (255, 0, 255), 1)
                 # cv2.rectangle(frame, (bbox[0], bbox[1]), ((bbox[0]+bbox[2]),(bbox[1]+bbox[3])), (255, 0, 255), 1)
-                
+        else:
+            bbox, box = [], []
+            self.center = None 
+            self.radius = None
+            self.area = None  
 
         return [frame, mask, self.center, self.radius, bbox, box, self.area]
 
 
 def main(args):
     image = cv2.imread("/home/jeremie/Frame_3.png")
-    # while 1:
+
+    object_detector = cv2.createBackgroundSubtractorMOG2()
+
     sc = ImageProcessing((29, 86, 6), (64, 255, 255))
     result = sc.process_image(image)
+    mask = object_detector.apply(image)
+    # while 1:
+    cv2.imshow("Mask background", mask)
     cv2.imshow("Frame", result[0])
     cv2.imshow("Mask", result[1])
     cv2.waitKey(1)
@@ -101,7 +111,7 @@ def main(args):
 
     if (cv2.waitKey(1) & 0xFF) == ord('q'):
         pass
-        #break
+    # break
 
     cv2.destroyAllWindows()
 
